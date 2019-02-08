1294f5329ccdc3c58029c7bcd2925253cd47faf7
asa1374 <asa1374@gmail.com>
2019-02-08 19:52:29 +0900

    2019.02.08 19:52

--- 물류사업 DB.sql
+++ 물류사업 DB.sql
@@ -2,17 +2,17 @@ select * from tab;
 CREATE TABLE Customers(
 customer_id VARCHAR2(15) PRIMARY KEY,
 customer_name VARCHAR2(15) NOT NULL,
-contact_name VARCHAR2(15) UNIQUE,
+password VARCHAR2(15) NOT NULL,
 address VARCHAR2(15) NOT NULL,
 city VARCHAR2(15) NOT NULL,
 postal_code VARCHAR2(15) NOT NULL,
-country VARCHAR2(15) NOT NULL
+ssn VARCHAR2(15) NOT NULL
 );
 CREATE TABLE Employees(
 employee_id VARCHAR2(15) PRIMARY KEY,
-last_name VARCHAR2(15) NOT NULL,
-first_name VARCHAR2(15) NOT NULL,
-birthdate VARCHAR2(15) UNIQUE,
+name VARCHAR2(15) NOT NULL,
+manager VARCHAR2(15) NOT NULL,
+birthdate VARCHAR2(15) NOT NULL,
 photo VARCHAR2(15),
 notes VARCHAR2(15)
 );
@@ -24,6 +24,8 @@ phone VARCHAR2(15)
 CREATE SEQUENCE order_id
 START WITH 1
 INCREMENT BY 1;
+
+
 
 CREATE TABLE Orders(
 order_id NUMBER PRIMARY KEY,
@@ -54,6 +56,7 @@ CREATE SEQUENCE category_id
 START WITH 1000
 INCREMENT BY 1;
 
+
 CREATE TABLE Categories(
 category_id NUMBER PRIMARY KEY,
 category_name VARCHAR2(15),
@@ -62,6 +65,7 @@ description VARCHAR2(15)
 CREATE SEQUENCE product_id
 START WITH 2000
 INCREMENT BY 1;
+
 
 CREATE TABLE Products(
 product_id NUMBER PRIMARY KEY,
@@ -90,12 +94,4 @@ CONSTRAINT Oder_Details_fk_Products FOREIGN KEY(product_id)
 quantity NUMBER DEFAULT 0
 );
 
-select * from customers;
-select * from employees;
-select * from shippers;
-select * from orders;
-select * from Oder_Details;
-select * from suppliers;
-select * from products;
-select * from categories;
 
--- 물류사업 SQL.sql
+++ 물류사업 SQL.sql
@@ -1,4 +1,4 @@
-SELECT * FROM EMPLOYEES;
+SELECT * FROM customers;
 ALTER TABLE EMPLOYEES RENAME COLUMN LAST_NAME TO MANAGER;
 
 create sequence emp_seq
@@ -8,10 +8,48 @@ create sequence emp_seq
 select * from user_sequences;
 
 insert into employees(EMPLOYEE_ID,MANAGER,NAME,BIRTHDATE,PHOTO,NOTES)
-VALUES(EMP_SEQ.NEXTVAL,'manager','김경민','960101','kim.jpg','매니저');
+VALUES(EMP_SEQ.NEXTVAL,'1000','이창준','930605-1','kim.jpg','매니저');
 
-select * from employees order by employee_id;
+delete from employees where name like '%';
+
+select * from employees;
 
 drop sequence emp_seq;
+
 delete from employees where manager like '김경민';
 select emp_seq.nextval from dual;
+
+SELECT * 
+FROM EMPLOYEES 
+WHERE EMPLOYEE_ID LIKE 1000 and name like '김경민';
+
+drop table customers;
+
+select * from customers;
+
+desc customers;
+alter table customers drop CONSTRAINT password;
+
+insert into customers(customer_id,customer_name,contact_name,address,city,postal_code,country)
+values('asa1374','이창준','자바신','경기도','파주시','1234','미국');
+
+alter table customers rename column country to ssn;
+
+
+select * from customers;
+select * from employees;
+select * from shippers;
+select * from orders;
+select * from suppliers;
+select * from categories;
+select * from products;
+select * from Oder_Details;
+
+drop table customers;
+drop table employees;
+drop table shippers;
+drop table orders;
+drop table suppliers;
+drop table categories;
+drop table products;
+drop table Oder_Details;
